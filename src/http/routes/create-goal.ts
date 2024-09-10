15f4cb7193d775ff4fe909f74df698667dc0f5bd
Miguel Souza <miguel.souza@gxmed.com.br>
2024-09-10 09:53:07 -0300

    Feat: Create Goal

---
+++ src/http/routes/create-goal.ts
@@ -0,0 +1,27 @@
+import { FastifyInstance } from "fastify"
+import { ZodTypeProvider } from "fastify-type-provider-zod"
+import { z } from "zod"
+import { createGoalFn } from "../../functions/create-goal-fn"
+
+export function createGoal(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    "/goals",
+    {
+      schema: {
+        body: z.object({
+          title: z.string(),
+          desiredWeeklyFrequency: z.number().int().min(1).max(7),
+        }),
+      },
+    },
+    async (request, reply) => {
+      const { title, desiredWeeklyFrequency } = request.body
+
+      const { goal } = await createGoalFn({ title, desiredWeeklyFrequency })
+
+      return reply.status(201).send({
+        goalId: goal.id,
+      })
+    }
+  )
+}
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,13 @@
 import fastify from "fastify"
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod"
 
 const app = fastify()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Server is running...")
